7596852b7dd171d73dd1be85411ff2cdd8133c25
Folombas <folombas@yandex.ru>
2025-05-07 20:24:24 +0300

    Exercise: Speeding up work

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
+// 10 конкурентных  запросов на GET по адресу google.com
+// Вывести в консоль 10 StatusCode
+
 func main() {
-	go printHi()
-	go fmt.Println("Hello from Main 2!")
-	go fmt.Println("Hello from Main!")
-	time.Sleep(time.Second)
+	t := time.Now()
+	for i := 0; i < 10; i++ {
+		go getHttpCode()
+	}
+	time.Sleep(time.Millisecond * 1100)
+	fmt.Println(time.Since(t))
 }
 
-func printHi() {
-	fmt.Println("Hello from Goruntine!")
+func getHttpCode() {
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Printf("Ошибка %s", err.Error())
+	}
+	fmt.Printf("Код: %d\n", resp.StatusCode)
 }
