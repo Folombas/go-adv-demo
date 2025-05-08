62ce37b64567df0ed34188262eba1217ad717b6d
Folombas <folombas@yandex.ru>
2025-05-08 22:35:50 +0300

    Goroutines is cool!

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func sumPart(arr []int, ch chan int) {
 }
 
 func main() {
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	numGoroutines := 3
 	ch := make(chan int, numGoroutines)
 
