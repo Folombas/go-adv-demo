99ea7f2cc6700ee904b1689df81bde5825b331e3
Folombas <folombas@yandex.ru>
2025-05-18 01:08:53 +0300

    Получение email из JWT

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -1,6 +1,7 @@
 package link
 
 import (
+	"go/adv-demo/configs"
 	"go/adv-demo/pkg/middleware"
 	"go/adv-demo/pkg/req"
 	"go/adv-demo/pkg/res"
@@ -10,9 +11,9 @@ import (
 	"gorm.io/gorm"
 )
 
-// Что такое JWT
 type LinkHandlerDeps struct {
 	LinkRepository *LinkRepository
+	Config 		   *configs.Config
 }
 
 type LinkHandler struct {
@@ -24,7 +25,7 @@ func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
 		LinkRepository: deps.LinkRepository,
 	}
 	router.HandleFunc("POST /link", handler.Create())
-	router.Handle("PATCH /link/{id}", middleware.IsAuthed(handler.Update()))
+	router.Handle("PATCH /link/{id}", middleware.IsAuthed(handler.Update(), deps.Config))
 	router.HandleFunc("DELETE /link/{id}", handler.Delete())
 	router.HandleFunc("GET /{hash}", handler.GoTo())
 }
